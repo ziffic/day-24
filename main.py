c28ba711dadc721b9a790bf9cc72f94f3dd3b834
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-14 14:59:52 -0700

    append and create new file

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# No need to close file with 'with'
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# to override a file
+# with open("my_file.txt", mode="w") as file:
+#     file.write("99828940 New URL")
+
+# to append a file
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\n45796654 sample")
+
+with open("new_file.txt", mode="w") as file:
+    file.write("\ncreate")
